e621fd4f3de51bff4b1c1e6759fffa0d4abc4da2
Ulises <ulises.argueta95@gmail.com>
2023-04-02 20:02:31 -0500

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (first) => {
+  let capitalize = tutorials.map((first) => {
+    first.split(" ");
+    console.log(first.split(" "));
+    return first
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.substring(1))
+    .join(" ");
+  });
+  return capitalize;
 }
